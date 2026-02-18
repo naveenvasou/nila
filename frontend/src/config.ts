bc3aa632579ebc551bce1a58529b062f8221388c
Naveen <naveenkumar7723nk@gmail.com>
2026-02-19 01:55:50 +0530

    fix password error

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "https://nila-backend-c2vff45ctq-el.a.run.app";
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "https://nila-backend-c2vff45ctq-el.a.run.app";
+// export const API_URL = "http://127.0.0.1:8000";
